08185a592beb0a685844f96f7d5bec7e7a6f00d0
Vashu <im.vashukashyap@gmail.com>
2023-07-03 09:13:26 +0530

    feature card created for desktop

--- src/Pages/FeaturedWork/FeaturedWork.jsx
+++ src/Pages/FeaturedWork/FeaturedWork.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import './featuredwork.css'
 import Subheading from '../../components/subheading/Subheading'
+import WorkCard from '../../components/WorkCard/WorkCard'
 
 export default function FeaturedWork() {
   return (
     <section className='featured padding_x'>
      <Subheading subheading={'#Featured Work'}/>
      <p>To be successful, the first thing to do is fall in love with your work.</p>
+     <div className="featured-wrok-container">
+      <WorkCard/>
+      <WorkCard/>
+      <WorkCard/>
+      <WorkCard/>
+     </div>
     </section>
   )
 }
---
+++ src/components/WorkCard/WorkCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import './workcard.css'
+
+export default function WorkCard() {
+  return (
+    <div className="workcard">
+      <div className="card-img">
+        <img src="./project/project-1.jpg" alt="project img" />
+      </div>
+      <div className="card-content">
+        <div className="content-container">
+        <h3>Designing Dashboards</h3>
+        <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+        </div>
+        <div className="btn-group-date">
+            <p>3 Jul 2023</p>
+            <div className="btn-group">
+                <button id="sharebtn">Share <i class="fa-solid fa-share-nodes"></i></button>
+                <button id="veiwbtn">Veiw Project <i class="fa-regular fa-eye"></i></button>
+                <button id="getcodebtn">Get Code <i class="fa-solid fa-code"></i></button>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
